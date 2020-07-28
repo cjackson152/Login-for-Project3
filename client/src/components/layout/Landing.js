febc1c38485f17e98b1ae74c3ea2f6a187d4f44c
Cole Jackson <cole_jackson@live.com>
2020-07-28 14:52:27 -0700

    works but for some reason register logs you in, appears to be because it passes the correct credentials and then catches on the jwt check

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -8,10 +8,10 @@ class Landing extends Component {
                 <div className = "row">
                     <div className = "col s12 center-align">
                         <h3>
-                            <b>Login</b>
+                            <b>PROJECT NAME</b>
                         </h3>
                         <p className="flow-text black-text text-darken-1">
-                            Register
+                            This is the LANDING PAGE
                         </p>
                         <br />
                         <div className="col s6">
@@ -24,8 +24,18 @@ class Landing extends Component {
                             }}
                             className="btn btn-large btn-flat waves-effect white black-text"
                             >
-                                login
+                                Register
                             </Link>
+                        </div>
+                        <div className="col s6">
+                            <Link 
+                                to="/login"
+                                style={{
+                                    width: "120px",
+                                    borderRadius: "5px",
+                                    letterSpacing: "2px"
+                                }}
+                                className="btn btn-large btn-flat">Login</Link>
                         </div>
                     </div>
                 </div>
